b8b94f64d74c90841a56d7a19b6c73f57bc2a623
prasadvenkat22 <prasadvenkat@hotmail.com>
2024-07-21 15:50:25 -0400

    Added a new homepage with a list of services offered by Empower BI.

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Layout from '@/components/Layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Welcome to Empower BI</h1>
+      <p>We specialize in the following services:</p>
+      <ul>
+        <li>Cloud consulting and support for Azure, AWS, GCP applications</li>
+        <li>Generative AI and Chat application development with RAG, Knowledge Graphs and Structured data</li>
+        <li>Database support offshore and onshore: Snowflake, Azure SQL, BigQuery, Databricks, MongoDB and Postgres</li>
+        <li>Programming and Application development in Python, Spark, SQL, Azure Data Factory, Databricks and Snowflake</li>
+        <li>Web application development and support in React.js, Next.js, HTML, Power BI, JavaScript and .NET</li>
+        <li>We provide 24/7 onshore and offshore support for web applications and backend</li>
+      </ul>
+    </Layout>
+  )
+}
