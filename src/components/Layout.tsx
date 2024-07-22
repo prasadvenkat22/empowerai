e7e1c92a01429211114705089fba2a5507c70071
prasadvenkat22 <prasadvenkat@hotmail.com>
2024-07-21 20:44:23 -0400

    Removed redundant code in the Layout component.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -22,27 +22,3 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 }
 
 export default Layout
-import React from 'react'
-import Link from 'next/link'
-
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <nav className="bg-gray-800 text-white">
-        <div className="container mx-auto px-4">
-          <ul className="flex space-x-4 py-4">
-            <li><Link href="/" className="hover:text-gray-300">Home</Link></li>
-            <li><Link href="/customer" className="hover:text-gray-300">Customer</Link></li>
-            <li><Link href="/service" className="hover:text-gray-300">Service</Link></li>
-            <li><Link href="/contacts" className="hover:text-gray-300">Contacts</Link></li>
-            <li><Link href="/users" className="hover:text-gray-300">Users</Link></li>
-            <li><Link href="/login" className="hover:text-gray-300">Login</Link></li>
-          </ul>
-        </div>
-      </nav>
-      <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
-    </div>
-  )
-}
-
-export default Layout
