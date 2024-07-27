80bc53b843524fc8356ea4852b0f8a2d83da246c
prasadvenkat22 <prasadvenkat@hotmail.com>
2024-07-27 18:03:09 -0400

    Removed duplicate `Image` import from Navbar component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import Image from "next/image";
-import Image from "next/image";
 
 type Tab = {
   name: string;
