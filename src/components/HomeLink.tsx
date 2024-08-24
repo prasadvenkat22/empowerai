912dbbce4fe27275b941feea106050307684b4d0
prasadvenkat22 <prasadvenkat@hotmail.com>
2024-08-24 16:50:12 -0400

    Added a fixed home link component with styling.

--- src/components/HomeLink.tsx
+++ src/components/HomeLink.tsx
@@ -9,3 +9,14 @@ const HomeLink = () => {
 }
 
 export default HomeLink
+import Link from 'next/link'
+
+const HomeLink = () => {
+  return (
+    <Link href="/" className="fixed top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Home
+    </Link>
+  )
+}
+
+export default HomeLink
