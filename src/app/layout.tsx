f54e5d7077b89c16e6c1d1110660152f5cac462f
prasadvenkat22 <prasadvenkat@hotmail.com>
2024-08-26 17:04:05 -0400

    Removed redundant navigation links from the layout component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from 'react';
 import TitleUpdater from './components/TitleUpdater';
 import Navbar from './components/Navbar';
 import UniqueHomeLink from '@/components/UniqueHomeLink';
-import Link from 'next/link';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const appName = "EBI"; // Define your app name here
@@ -15,16 +14,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <TitleUpdater appName={appName} /> {/* Include TitleUpdater */}
         <Navbar />
         <UniqueHomeLink />
-        <nav className="bg-gray-100 p-4">
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/" className="text-blue-500 hover:text-blue-700">Home</Link>
-            </li>
-            <li>
-              <Link href="/registration-form" className="text-blue-500 hover:text-blue-700">Registration Form</Link>
-            </li>
-          </ul>
-        </nav>
         {children}
       </body>
     </html>
