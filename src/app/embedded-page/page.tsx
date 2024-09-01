daf52b4942d295597625baec6d1b4069b5939fb7
prasadvenkat22 <prasadvenkat@hotmail.com>
2024-09-01 18:11:52 -0400

    Changed the iframe source to use HTTPS instead of HTTP.

--- src/app/embedded-page/page.tsx
+++ src/app/embedded-page/page.tsx
@@ -4,11 +4,12 @@ const EmbeddedPage: React.FC = () => {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="http://165.227.97.62:8501/"
+        src="https://165.227.97.62:8501/"
         width="100%"
         height="100%"
         style={{ border: 'none' }}
         title="Embedded Content"
+        sandbox="allow-scripts allow-same-origin"
       />
     </div>
   );
