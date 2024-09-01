e1b563d4cd9b9b16d43d3580511037cf40452738
prasadvenkat22 <prasadvenkat@hotmail.com>
2024-09-01 18:12:00 -0400

    Embedded page component updated to use full screen iframe with no border.

--- src/app/embedded-page/page.tsx
+++ src/app/embedded-page/page.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const EmbeddedPage: React.FC = () => {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen flex">
       <iframe
         src="https://165.227.97.62:8501/"
-        width="100%"
-        height="100%"
-        style={{ border: 'none' }}
+        className="w-full h-full border-none"
         title="Embedded Content"
         sandbox="allow-scripts allow-same-origin"
       />
